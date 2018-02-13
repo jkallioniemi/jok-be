1908614c6fbcf2f62659d2298efc4e918738b51f
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-02-13 20:52:06 +0200

    Create migration for initial schema
    Also installed knex and pg packages.

---
+++ db/migrations/20180213191019_initial_schema.js
@@ -0,0 +1,22 @@
+exports.up = knex => knex.schema
+  .createTable('Species', (table) => {
+    table.increments('id').unsigned().primary();
+    table.string('name').notNull();
+  })
+  .createTable('Sighting', (table) => {
+    table.increments('id').unsigned().primary();
+    table
+      .integer('speciesId')
+      .unsigned()
+      .references('id')
+      .inTable('Species');
+    table.timestamp('dateTime').default(knex.fn.now()).notNull();
+    table.text('description').nullable();
+    table.integer('count').notNull();
+  })
+  .raw(`CREATE SCHEMA IF NOT EXISTS extensions;
+        CREATE EXTENSION IF NOT EXISTS postgis SCHEMA extensions;`);
+
+exports.down = knex => knex.schema
+  .dropTableIfExists('People')
+  .dropTableIfExists('Species');
---
+++ knexfile.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+require('dotenv-safe').load({
+  path: path.join(__dirname, './.env'),
+  sample: path.join(__dirname, './.env.example'),
+});
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      port: process.env.JOK_DATABASE_PORT,
+      host: process.env.JOK_DATABASE_HOST,
+      database: process.env.JOK_DATABASE_NAME,
+      user: process.env.JOK_DATABASE_USER,
+      password: process.env.JOK_DATABASE_PASSWORD,
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'knex_migrations',
+    },
+  },
+
+};
