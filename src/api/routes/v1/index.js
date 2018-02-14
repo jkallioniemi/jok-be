58ac55c1227cb72d54a335f8d08dad98d126bca2
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-02-14 23:55:12 +0200

    Fix seed data, add route to get and post data

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,3 +1,4 @@
+const Sighting = require('../../models/Sighting');
 const express = require('express');
 
 const router = express.Router();
@@ -6,5 +7,25 @@ const router = express.Router();
  * GET v1/status
  */
 router.get('/status', (req, res) => res.send('OK'));
+
+router.get('/sightings', async (req, res) => {
+  const sightings = await Sighting.query();
+  res.send(sightings);
+});
+
+router.post('/sightings', async (req, res) => {
+  const newSighting = {
+    speciesId: req.body.species,
+    description: req.body.description,
+    dateTime: req.body.dateTime,
+    count: req.body.count,
+  };
+
+  await Sighting
+    .query()
+    .insert(newSighting)
+    .then(result => res.send(result))
+    .catch(err => res.status(418).send(err));
+});
 
 module.exports = router;
